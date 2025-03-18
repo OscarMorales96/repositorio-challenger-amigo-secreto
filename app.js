b71744cd0cef8a362f83686b129a0b5a87292629
OscarMorales96 <oscar.morales2e@gmail.com>
2025-03-18 01:46:26 -0500

    Implementando una función para agregar amigos y otra para actualizar amigos

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaDeAmigos = [];
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo').value.trim();
+
+    if(nombreAmigo === "") {
+        alert('Por favor, ingrese un nombre.');
+        return;
+    }
+
+    listaDeAmigos.push(nombreAmigo);
+   
+    document.getElementById('amigo').value = "";
+
+    actualizarListaDeAmigos();
+}
+
+function actualizarListaDeAmigos(){
+    let listaHTML = document.getElementById('listaAmigos');
+    listaHTML.innerHTML = "";
+
+    for (let amigo of listaDeAmigos) {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        listaHTML.appendChild(li);
+    }
+}
